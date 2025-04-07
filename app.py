6fdb1ea5026182e889c6cee04903d013e0ecb8e9
Apurba1903 <apurba1903@gmail.com>
2025-04-07 16:10:29 +0600

    completed the skeleton

---
+++ app.py
@@ -0,0 +1,37 @@
+import streamlit as st
+import pandas as pd
+
+df = pd.read_csv('startup_funding.csv')
+
+# Data Cleaning
+df["Investors Name"] = df["Investors Name"].fillna('Undisclosed')
+
+
+st.sidebar.title('Startup Funding Analysis')
+
+option = st.sidebar.selectbox('Select One', ['Overall Analysis', 'Startup', 'Investor'])
+
+if option == 'Overall Analysis':
+    st.title('Overall Analysis')
+
+
+elif option == 'Startup':
+    st.sidebar.selectbox('Select Startup', sorted(df["Startup Name"].unique().tolist()))
+    btn1 = st.sidebar.button('Find Startup Details')
+    st.title('Startup Analysis')
+
+
+else:
+    st.sidebar.selectbox('Select Investor', sorted(df["Investors Name"].unique().tolist()))
+    btn2 = st.sidebar.button('Find Investor Details')
+    st.title('Investor Analysis')
+
+
+
+
+
+
+
+
+
+
